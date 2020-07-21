b282c210736c0fcb9fe5d3c20c0ad1b4dbf215d3
domosute <domosute@users.noreply.github.com>
2020-07-21 09:42:23 -0500

    Create build.sh

---
+++ conda_recipe/quantlib/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+MULTI_THREAD=4
+export PREFIX=/opt/conda
+export CFLAGS="${CFLAGS} -I${PREFIX}/include"
+export CXXFLAGS="${CFLAGS}"
+export CPPFLAGS="-I${PREFIX}/include"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+
+./autogen.sh
+./configure --prefix=$PREFIX
+make -j ${MULTI_THREAD}
+sudo make install
